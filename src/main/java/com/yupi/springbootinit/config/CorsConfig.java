952f681f01d9b44a4ffce2ab1a85d77b3c06ff66
hechenglong <hechenglong@xiaohongshu.com>
2024-08-27 17:52:27 +0800

    跨域配置项改动

--- src/main/java/com/yupi/springbootinit/config/CorsConfig.java
+++ src/main/java/com/yupi/springbootinit/config/CorsConfig.java
@@ -1,27 +1,22 @@
-        package com.yupi.springbootinit.config;
+package com.yupi.springbootinit.config;
 
-        import org.springframework.context.annotation.Configuration;
-        import org.springframework.web.servlet.config.annotation.CorsRegistry;
-        import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-        /**
-         * 全局跨域配置-解决方案不优雅-后期通过NGINX处理
-         *
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
 
-         */
-        @Configuration
-        public class CorsConfig implements WebMvcConfigurer {
-
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                // 覆盖所有请求
-                registry.addMapping("/**")
-                        // 允许发送 Cookie
-                        .allowCredentials(true)
-                        // 放行哪些域名（必须用 patterns，否则 * 会和 allowCredentials 冲突）
-                        .allowedOriginPatterns("http://120.76.47.158:8200","http://47.121.31.131:80")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .exposedHeaders("*");
-            }
-        }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 覆盖所有请求  
+        registry.addMapping("/**")
+                // 允许发送 Cookie  
+                .allowCredentials(true)
+                // 放行哪些域名  
+                .allowedOriginPatterns("http://120.76.47.158:8200", "http://47.121.31.131:*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+}
